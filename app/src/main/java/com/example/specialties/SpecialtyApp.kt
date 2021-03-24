159a07c8ec2206f063ab772b7b682226dff09dc1
Лагун Виктор Олегович <lagun.viktor@otr.ru>
2021-03-24 19:12:29 +0300

    Implemented navigation

--- app/src/main/java/com/example/specialties/SpecialtyApp.kt
+++ app/src/main/java/com/example/specialties/SpecialtyApp.kt
@@ -2,13 +2,20 @@ package com.example.specialties
 
 import android.app.Application
 import com.example.specialties.di.DaggerAppComponent
+import io.reactivex.plugins.RxJavaPlugins
 
 
 class SpecialtyApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        init()
+
+    }
+
+    private fun init() {
         DaggerAppComponent.builder().application(this)
             .build().inject(this)
+        RxJavaPlugins.setErrorHandler { it.printStackTrace() }
     }
 }
